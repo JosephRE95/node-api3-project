cebf2a3008a4739ba4c4e3906d944740e49cd442
Joseph Ellis <josephellis95@gmail.com>
2024-04-11 14:28:17 -0500

    The PUT is working but not within postman...

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -10,8 +10,7 @@ const {
 const User = require('./users-model')
 const Post = require('../posts/posts-model')
 
-// You will need `users-model.js` and `posts-model.js` both
-// The middleware functions also need to be required
+
 
 const router = express.Router();
 
@@ -35,11 +34,15 @@ router.post('/', validateUser, (req, res, next) => {
   .catch(next)
 });
 
-router.put('/:id',validateUserId, validateUser, (req, res) => {
-  // RETURN THE FRESHLY UPDATED USER OBJECT
-  // this needs a middleware to verify user id
-  // and another middleware to check that the request body is valid
-  console.log(req.user)
+router.put('/:id', validateUserId, validateUser, (req, res, next) => {
+ User.update(req.params.id, { name : req.name })
+ .then(() => {
+  return User.getById(req.params.id)
+ })
+ .then(user => {
+  res.json(user)
+ })
+ .catch(next)
 });
 
 router.delete('/:id',validateUserId, (req, res) => {
